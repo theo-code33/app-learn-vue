3055cc290450559eacb467c8ed68abe049bfea87
Théo Gillet <gillettheo1@gmail.com>
2022-06-02 11:59:44 +0200

    add two component

---
+++ src/components/FilmsList.vue
@@ -0,0 +1,14 @@
+<script>
+export default {
+    name: 'FilmsList',
+    props: ["filmsList"],
+}
+</script>
+
+<template>
+    <ul>
+        <li v-for="film in filmsList" :key="film.title">
+            {{ film.title }}
+        </li>
+    </ul>
+</template>
---
+++ src/components/FormContact.vue
@@ -0,0 +1,44 @@
+<template>
+    <form @submit.prevent="handleSubmit">
+        <input type="text" placeholder="Le titre du site" v-model="credentials.title">
+        <input type="text" placeholder="Durée du film" v-model="credentials.duration">
+        <input type="text" placeholder="Notation du film" v-model="credentials.notation">
+        <input type="submit" value="Envoyer">
+    </form>
+
+    <FilmsList :filmsList="filmsList" />
+</template>
+
+<script>
+import FilmsList from "@/components/FilmsList.vue";
+
+export default {
+    name: 'FormContact',
+    components: {
+        FilmsList
+    },
+    data(){
+        return {
+            credentials: {
+                title: '',
+                duration: '',
+                notation: '',
+            },
+            filmsList: []
+        }
+    },
+    methods:{
+        handleSubmit(){
+            console.log("Credential ",this.credentials);
+            console.log("FilmsList",this.filmsList);
+            this.filmsList.push(this.credentials);
+
+            this.credentials = {
+                title: '',
+                duration: '',
+                notation: '',
+            }
+        }
+    }
+}
+</script>
